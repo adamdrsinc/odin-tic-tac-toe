900cfad821bf876386adc3c8c326e559698899dd
adamdrsinc <adam.sinc124@gmail.com>
2025-08-29 21:07:46 +0100

    add run(), equivalent to main in cpp. Add player factory, gameboard & gameMaster IIFEs

---
+++ js/script.js
@@ -0,0 +1,44 @@
+function createPlayer(name, marker){
+    return {name, marker};
+}
+
+function run(){
+    const gameboard = (function(){
+        let board = [];
+
+        const checkForWinner = function(){
+            
+        }
+
+        return {board};
+    })();
+
+    const gameMaster = (function(){
+
+    })();
+
+}
+
+run();
+
+/*
+
+XXX
+XXX
+XXX
+
+123
+456
+789
+
+Check 123, then 456, then 789
+
+Check 147, then 258, then 369
+
+Check 159, then 357
+
+*/
+
+function checkForWinner(gameboard){
+
+}
