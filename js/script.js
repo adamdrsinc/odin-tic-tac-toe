0a0adccfcc15eaf2b007f9b8508fd60a300e7b28
adamdrsinc <adam.sinc124@gmail.com>
2025-08-29 22:16:53 +0100

    Finish game logic for console use

--- js/script.js
+++ js/script.js
@@ -6,10 +6,23 @@ function run(){
     const player1 = createPlayer("Adam", "X");
     const player2 = createPlayer("Ben", "O");
 
-    const gameboard = (function(){
-        let board = Array(9).fill("");
+    let currentPlayer = player1;
 
-        return {board};
+    const gameboard = (function(){
+        let board = [1,2,3,4,5,6,7,8,9];
+
+        const boardFormattedAsString = function(){
+            return `${board[0]} ${board[1]} ${board[2]}\n${board[3]} ${board[4]} ${board[5]}\n${board[6]} ${board[7]} ${board[8]}`;
+        }
+
+        const placeMarker = function(marker, spotChoice){
+            if(board[spotChoice] === "X" || board[spotChoice] === "O") return false;
+
+            board[spotChoice] = marker;
+            return true;
+        }
+
+        return {board, placeMarker, boardFormattedAsString};
     })();
 
     const gameMaster = (function(){
@@ -32,27 +45,67 @@ function run(){
                 return {winningMarker: board[2]};
             }
 
+            //Check for tie
+            let isTie = true;
+            for(let i = 0; i < 9; i++){
+                if(board[i] !== "X"){
+                    if(board[i] !== "O"){
+                        isTie = false;
+                    }
+                }
+            }
+
+            if(isTie){
+                return "TIE";
+            }
+
             return null;
         }
 
-        const playRound = function(board){
-            
+        const playRound = function(player, board){
+            function askPlayerForMarkerPlacement(player){
+                let input = -1;
+                while(isNaN(input) || input < 1 || input > 9){
+                    input = parseInt(prompt(`${player.name}'s turn`));
+                }
+
+                return input;
+            }
+
+            let playerInput = askPlayerForMarkerPlacement(player) - 1;
+            let markerPlacementResult = board.placeMarker(player.marker, playerInput);
+            while(!markerPlacementResult){
+                playerInput = askPlayerForMarkerPlacement(player) - 1;
+                markerPlacementResult = board.placeMarker(player.marker, playerInput);
+            }
         }
 
-        return {checkForWinner};
+        return {playRound, checkForWinner};
     })();
 
     while(true){
-        gameMaster.playRound(gameboard.board);
+        console.log(gameboard.boardFormattedAsString());
+        console.log();
+
+        gameMaster.playRound(currentPlayer, gameboard);
 
         const winnerResult = gameMaster.checkForWinner(gameboard.board);
 
         if(!!winnerResult){
-            const winningPlayer = player1.marker === winnerResult.winningMarker ? player1.name : player2.name;
-            console.log(`${winningPlayer} wins!`);
+            if(winnerResult === "TIE"){
+                console.log(`There has been a tie.`);
+                console.log(`Tying Board:`);
+                console.log(gameboard.displayBoard());
+            } else {
+                const winningPlayer = player1.marker === winnerResult.winningMarker ? player1.name : player2.name;
+                console.log(`${winningPlayer} wins!`);
+                console.log(`Winning Board:`);
+                gameboard.displayBoard();
+            }
+
             break;
         } 
+
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
     }
 }
-
-run();
